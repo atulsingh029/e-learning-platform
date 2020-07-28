cff167d912778cc03d2c07863e3047d2388aaed7
admin@atul <atulsingh29@protonmail.com>
2020-07-28 13:30:10 +0530

    added : username function[backlogged]

--- custom_user/views.py
+++ custom_user/views.py
@@ -23,12 +23,16 @@ def signup(request):
 
 def generate_username(email):
     response = ''
-    # author: abhishek
     # you are given a str email you need return username from that email
     # eg: atul@email.com, so you have to return 'atul' through response variable
-    ''' use function User.objects.filter(username = 'atul') i.e. username is the one you just extracted, if this method 
-     returns empty queryset then return same username eg. 'atul' but if you get non empty queryset then append a number
-     at end of username eg. 'atul_1' because username is primary key in database and it should not be duplicated '''
+    atIndex = email.index('@')
+    response = email[0:atIndex]
+    checker = User.objects.filter(username=response)
+    count = 1
+    while(len(checker) != 0):
+        response = response + str(count)
+        checker = User.objects.filter(username=response)
+        count = count + 1
     return response
 
 
