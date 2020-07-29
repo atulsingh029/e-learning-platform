2a4aac714cb14bf65c0677333b32290fc6cee6f0
admin@atul <atulsingh29@protonmail.com>
2020-07-29 13:00:44 +0530

    fix

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_learning_plateform.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_learning_platform.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
