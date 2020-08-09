0ec9ca75aff6585700d5fab0833530e1b87eb117
admin@atul <atulsingh29@protonmail.com>
2020-08-09 19:10:18 +0530

    bug fix

--- custom_user/models.py
+++ custom_user/models.py
@@ -44,7 +44,7 @@ class ApplyForStudent(models.Model):
     password = models.CharField(max_length=512)
     reference = models.CharField(max_length=100,default=0)
     submissionstamp = models.DateTimeField(auto_now=True)
-    for_room = models.ForeignKey(Room,on_delete=models.SET(1),default=1)
+    for_room = models.ForeignKey(Room,on_delete=models.SET(1),default=Room.objects.get(id=1))
     status = models.BooleanField(default=False)
 
     def __str__(self):
