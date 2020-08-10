536fdb9ea110030aa64aa6d2285c8666dbfdd6ed
abhishekjais <abhishek.jaiswal_cs18@gla.ac.in>
2020-08-11 00:44:32 +0530

    updated api/serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -6,20 +6,43 @@ from management.models import Course, Lecture, CourseResource, LectureResource
 class ApplicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = ApplyForStudent
-        fields = ['id','first_name','last_name','email','phone','reference','submissionstamp','status']
+        fields = ['id', 'first_name', 'last_name', 'email', 'phone', 'reference', 'submissionstamp', 'status']
 
 
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ['id','title', 'description', 'display_pic', 'room_stream_details', 'organization']
+        fields = ['id', 'title', 'description', 'display_pic', 'room_stream_details', 'organization']
 
 
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = ['id','username', 'email', 'first_name', 'last_name']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
 
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ['c_id', 'c_name', 'c_description', 'for_organization', 'c_status']
+
+
+class CourseResourceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CourseResource
+        fields = ['cr_name', 'cr_description', 'for_course', 'cr_url']
+
+
+class LectureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lecture
+        fields = ['for_course', 'l_number', 'l_name', 'l_url']
+
+
+class LectureResourceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LectureResource
+        fields = ['lr_name', 'lr_description', 'for_lecture', 'lr_url']
 
 # @abhishek
 # write serializers for following models :
