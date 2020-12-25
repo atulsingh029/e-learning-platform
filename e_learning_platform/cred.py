be07f3a1d9860d60e577a446775212badd476969
Atul Singh <atulsinghash@gmail.com>
2020-12-25 18:51:24 +0530

    Update cred.py

--- e_learning_platform/cred.py
+++ e_learning_platform/cred.py
@@ -4,8 +4,8 @@ secret_key = 'secret key here'
 
 # SMTP setup
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'primestudies.glau@gmail.com'
-EMAIL_HOST_PASSWORD = 'fJca57ezPseRSpp'
+EMAIL_HOST_USER = 'service email here'
+EMAIL_HOST_PASSWORD = 'service email password here'
 EMAIL_PORT = 587
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
